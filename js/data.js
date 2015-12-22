18fc39ddbf34bb7c7b1756fcd750d7a01c06cd1c
Sławomir Domagała <slawomir.karol.domagala@gmail.com>
2015-12-22 13:20:16 +0100

    Added 3rd account

--- js/data.js
+++ js/data.js
@@ -14,19 +14,29 @@ define([
 				"transactions_to": [
 					{
 						"title": "Początkowa wpłata",
-						"value": 500.0,
+						"value": 2500.0,
 						"to_name": "Joanna Struga-Okoń",
 						"to_address": "Warszawa 00-000, ul. Tysiąclecia 2",
-						"to_number": 891000342234221123
+						"to_number": 891000342234221123,
+						"date": '2015-12-12'
+					},
+					{
+						"title": "Początkowa wpłata",
+						"value": 2500.0,
+						"to_name": "Joanna Struga-Okoń",
+						"to_address": "Warszawa 00-000, ul. Tysiąclecia 2",
+						"to_number": 723420343453421123,
+						"date": '2015-12-12'
 					}
 				],
 				"transactions_from": [
 					{
 						"title": "Początkowa wpłata",
-						"value": 1000.0,
+						"value": 10000.0,
 						"from_name": "Jan Kowalski",
 						"from_address": "Warszawa 00-000, ul. Tysiąclecia 1",
-						"from_number": 232348091343298430
+						"from_number": 232348091343298430,
+						"date": '2015-11-12'
 					}
 				]
 			},
@@ -38,10 +48,27 @@ define([
 				"transactions_from": [
 					{
 						"title": "Początkowa wpłata",
-						"value": 500.0,
+						"value": 2500.0,
 						"from_name": "Joanna Struga-Okoń",
 						"from_address": "Warszawa 00-000, ul. Tysiąclecia 2",
-						"from_number": 891000342234221123
+						"from_number": 891000342234221123,
+						"date": '2015-12-12'
+					}
+				]
+			},
+			{
+				"name": "Rachunek oszczędnościowy 'Dziecko'",
+				"number": 723420343453421123,
+				"id": 3,
+				"transactions_to": [],
+				"transactions_from": [
+					{
+						"title": "Początkowa wpłata",
+						"value": 2500.0,
+						"from_name": "Joanna Struga-Okoń",
+						"from_address": "Warszawa 00-000, ul. Tysiąclecia 2",
+						"from_number": 891000342234221123,
+						"date": '2015-12-12'
 					}
 				]
 			}
